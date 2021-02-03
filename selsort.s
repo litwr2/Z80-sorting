5ee82cd5fae757a57743f5555b336f95e4b0cc95
micro <litwr@yandex.ru>
2021-02-03 13:10:55 +0300

    selection sort the same improvement fo ESZ=2

--- selsort.s
+++ selsort.s
@@ -44,9 +44,8 @@ endif
           inc de
           jp .ll3
 
-.ll8:
+.ll8:     ld b,(hl)
 if ESZ=1
-          ld b,(hl)
           ex (sp),hl
           ld a,(hl)
           ld (hl),b
@@ -55,20 +54,19 @@ if ESZ=1
           ld (hl),a
           pop hl
 else
-          ld (.m1a+1),hl
-          ld (.m1b+1),hl
-          pop hl
-          push hl
-          ld (.m2a+1),hl
-          ld (.m2b+1),hl
-.m1a      ld hl,(0)
-          push hl
-.m2a      ld hl,(0)
-.m1b      ld (0),hl
-          pop hl
-.m2b      ld (0),hl
-          pop hl
           inc l
+          ld c,(hl)
+          ex (sp),hl
+          ld a,(hl)
+          ld (hl),b
+          inc l
+          ld b,(hl)
+          ld (hl),c
+          ex (sp),hl
+          ld (hl),b
+          dec l
+          ld (hl),a
+          pop hl
           inc hl
 endif
           push hl
